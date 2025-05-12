7b7e9c278103f94f96ec25b355dcfca9733d962b
BGSIBotting <charbelbahaboza@gmail.com>
2025-05-12 14:53:03 -0400

    Create rifts.lua

---
+++ extra/rifts.lua
@@ -0,0 +1,11 @@
+local rifts = {
+  ["royal-chest"] = "Special",
+  ["dice-rift"] = "Special",
+  ["underworld-1"] = "Special",
+  ["underworld-2"] = "Special",
+  ["underworld-3"] = "Special",
+  ["rainbow-egg"] = "Island",
+  ["void-egg"] = "Island",
+  ["nightmare-egg"] = "Island",
+  ["cyber-egg"] = "Island",
+}
